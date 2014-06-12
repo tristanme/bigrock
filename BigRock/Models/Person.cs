ddd6c3e432ca61dd3f0ffc21ace78f2183f2f4f2
tristanme <tristan.mendenhall@gmail.com>
2014-06-11 21:01:38 -0400

    Adding support for calculated display name for people.

--- BigRock/Models/Person.cs
+++ BigRock/Models/Person.cs
@@ -1,9 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.ComponentModel;
+using System.Text;
 
 namespace BigRock.Models
 {
+    public class BigRockConnection : DbContext
+    {
+        public DbSet<Person> Person { get; set; }
+    }
+
     public class Person
     {
         public enum TypeCodes
@@ -20,7 +26,7 @@ namespace BigRock.Models
             [Description("Other")]
             Other
         }
- 
+
         [Key]
         public int ID { get; set; }
 
@@ -37,10 +43,20 @@ namespace BigRock.Models
         [Required]
         public string LastName { get; set; }
 
+        [Display(Name = "Name")]
+        [DataType(DataType.Text)]
+        public string DisplayName
+        {
+            get
+            {
+                return BuildDisplayName();
+            }
+        }
+
         [Display(Name = "Family code")]
         [DataType(DataType.Text)]
         [StringLength(4, MinimumLength = 4)]
-        [RegularExpression(@"^\d+$",ErrorMessage = "The family code must be exactly four numbers each between 0-9.")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "The family code must be exactly four numbers each between 0-9.")]
         [Required]
         public string FamilyCode { get; set; }
 
@@ -56,10 +72,36 @@ namespace BigRock.Models
         [Display(Name = "Other")]
         [DataType(DataType.Text)]
         public string TypeCodeOther { get; set; }
+
+        #region Helper functions
+
+        private string BuildDisplayName()
+        {
+           // TODO: add support for name formatting functions
+            StringBuilder nameBuilder = new StringBuilder();
+
+            if (!string.IsNullOrEmpty(this.FirstName))
+            {
+                nameBuilder.Append(this.FirstName);
+            }
+
+            if (!string.IsNullOrEmpty(this.MiddleName))
+            {
+                nameBuilder.Append(string.Format(" {0}.", this.MiddleName.Substring(0, 1)));
+            }
+
+            if (nameBuilder.Length > 0)
+            {
+                nameBuilder.Append(" ");
+            }
+
+            nameBuilder.Append(this.LastName);
+
+            return nameBuilder.ToString();
+
+        }
+
+        #endregion
     }
 
-    public class BigRockConnection : DbContext
-    {
-        public DbSet<Person> Person { get; set; }
-    }
 }
